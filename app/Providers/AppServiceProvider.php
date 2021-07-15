0aee4766689f7f98ed15e239cc3ef5c0adffee08
Alex <1080810@alumno.um.edu.mx>
2021-07-15 16:23:16 -0500

    new https aproach for solving mail confirmation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -67,8 +67,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('APP_ENV') !== 'local'){
+        /*if(env('APP_ENV') !== 'local'){
             URL::forceScheme('https');
-        }
+        }*/
     }
 }
